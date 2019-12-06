7e1532939d118f9441886bfece209c6fac88b622
ghsmith <geoffrey.hughes.smith@gmail.com>
2019-12-06 18:38:12 -0500

    Release candidate.

--- merge.sh
+++ merge.sh
@@ -18,3 +18,21 @@ tail -n+4 mm_copath_patient_list-part_af.export4dj.xml | head -n-2 >> mm_copath_
 tail -n+4 mm_copath_patient_list-part_ag.export4dj.xml | head -n-2 >> mm_copath_patient_list.export4dj.xml
 tail -n+4 mm_copath_patient_list-part_ah.export4dj.xml >> mm_copath_patient_list.export4dj.xml
 
+cat mm_copath_patient_list-part_aa.export4dj.with_demographics.csv > mm_copath_patient_list.export4dj.with_demographics.csv
+tail -n+2 mm_copath_patient_list-part_ab.export4dj.with_demographics.csv >> mm_copath_patient_list.export4dj.with_demographics.csv
+tail -n+2 mm_copath_patient_list-part_ac.export4dj.with_demographics.csv >> mm_copath_patient_list.export4dj.with_demographics.csv
+tail -n+2 mm_copath_patient_list-part_ad.export4dj.with_demographics.csv >> mm_copath_patient_list.export4dj.with_demographics.csv
+tail -n+2 mm_copath_patient_list-part_ae.export4dj.with_demographics.csv >> mm_copath_patient_list.export4dj.with_demographics.csv
+tail -n+2 mm_copath_patient_list-part_af.export4dj.with_demographics.csv >> mm_copath_patient_list.export4dj.with_demographics.csv
+tail -n+2 mm_copath_patient_list-part_ag.export4dj.with_demographics.csv >> mm_copath_patient_list.export4dj.with_demographics.csv
+tail -n+2 mm_copath_patient_list-part_ah.export4dj.with_demographics.csv >> mm_copath_patient_list.export4dj.with_demographics.csv
+
+head -n-2 mm_copath_patient_list-part_aa.export4dj.with_demographics.xml > mm_copath_patient_list.export4dj.with_demographics.xml
+tail -n+4 mm_copath_patient_list-part_ab.export4dj.with_demographics.xml | head -n-2 >> mm_copath_patient_list.export4dj.with_demographics.xml
+tail -n+4 mm_copath_patient_list-part_ac.export4dj.with_demographics.xml | head -n-2 >> mm_copath_patient_list.export4dj.with_demographics.xml
+tail -n+4 mm_copath_patient_list-part_ad.export4dj.with_demographics.xml | head -n-2 >> mm_copath_patient_list.export4dj.with_demographics.xml
+tail -n+4 mm_copath_patient_list-part_ae.export4dj.with_demographics.xml | head -n-2 >> mm_copath_patient_list.export4dj.with_demographics.xml
+tail -n+4 mm_copath_patient_list-part_af.export4dj.with_demographics.xml | head -n-2 >> mm_copath_patient_list.export4dj.with_demographics.xml
+tail -n+4 mm_copath_patient_list-part_ag.export4dj.with_demographics.xml | head -n-2 >> mm_copath_patient_list.export4dj.with_demographics.xml
+tail -n+4 mm_copath_patient_list-part_ah.export4dj.with_demographics.xml >> mm_copath_patient_list.export4dj.with_demographics.xml
+
